7eefa805bc0663cd4c03e3de467c4910c70024ca
Patrick Watters <patrickwatters@Mac.cable.rcn.com>
2025-09-27 12:21:47 -0400

    \r\n

--- webServer.py
+++ webServer.py
@@ -32,7 +32,7 @@ def webServer(port=13331):
       #This variable can store the headers you want to send for any valid or invalid request.   What header should be sent for a response that is ok?    
       #Fill in start 
       data = f.read()
-      headers = ("HTTP/1.1 200 OK\r\n" "Content-Type: text/html; charset=utf-8\r\n" f"Content-Length: {len(data.encode('utf-8'))}\r\n" "Connection: keep-alive" "Server: mine" "\r\n")
+      headers = ("HTTP/1.1 200 OK\r\n" "Content-Type: text/html; charset=utf-8\r\n" f"Content-Length: {len(data.encode('utf-8'))}\r\n" "Connection: keep-alive\r\n" "Server: mine\r\n" "\r\n")
               
       #Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
  
@@ -51,12 +51,12 @@ def webServer(port=13331):
         
       connectionSocket.close() #closing the connection socket
       
-    except (FileNotFoundError, Exception):
+    except Exception:
       # Send response message for invalid request due to the file not being found (404)
       # Remember the format you used in the try: block!
       #Fill in start 
       data = "<h1>404 Not Found</h1>"
-      headers = ("HTTP/1.1 404 Not Found\r\n" "Content-Type: text/html; charset=utf-8\r\n" "Content-Length: {len(data.encode('utf-8'))}\r\n" "Connection: keep-alive" "Server: mine" "\r\n")
+      headers = ("HTTP/1.1 404 Not Found\r\n" "Content-Type: text/html; charset=utf-8\r\n" "Content-Length: {len(data.encode('utf-8'))}\r\n" "Connection: keep-alive\r\n" "Server: mine\r\n" "\r\n")
       #Fill in end
 
 
